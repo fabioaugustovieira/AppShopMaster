485e8ea615576deaedfc2c6a7d3e51d9b975ad93
fabioaugustovieira <168941490+fabioaugustovieira@users.noreply.github.com>
2026-05-08 08:32:18 -0300

    Add files via upload

---
+++ View/ProfileView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Perfil do usuário")
+            .font(.title)
+            .navigationTitle("Perfil")
+    }
+}
